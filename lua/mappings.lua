708e3480fa31cae504d696e8308f2b407c896ba5
Grady Wong <grady.w@xtaotech.com>
2025-02-25 11:28:58 +0800

    add some keymap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,6 +8,16 @@ local autocmd = vim.api.nvim_create_autocmd
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 map("n", "<leader>sg", require('telescope.builtin').live_grep, {desc = '[S]earch [G]rep'})
+map("n", "<leader>sr", require('telescope.builtin').resume, {desc = '[S]earch [R]esume'})
+map("n", "<leader>s.", require('telescope.builtin').oldfiles, {desc = '[S]earch Recent Files'})
+map("n", "<leader><leader>", require('telescope.builtin').buffers, {desc = '[ ] Find Existing Buffers'})
+map('n', '<leader>/', function()
+        -- You can pass additional configuration to Telescope to change the theme, layout, etc.
+        require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
 
 autocmd('LspAttach', {
   callback = function (args)
